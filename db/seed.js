5bc36a550a05e8281cc049503f177d5fc5d8f293
dp-daly <dan.daly@education.gov.uk>
2024-06-18 20:23:49 +0100

    added seeding data and more css

--- db/seed.js
+++ db/seed.js
@@ -11,14 +11,44 @@ async function seed() {
       await truncateDb()
       console.log('🖌️  Database Dropped 🖌️')
   
-      const adminUser = await User.create({
+      const users = await User.create({
+        firstName: 'Admin',
         username: 'admin',
         email: 'admin@email.com',
         password: 'pass',
         isAdmin: true,
-      })
+      },
+      {
+        firstName: 'Emma',
+        username: 'efrith',
+        email: 'emma.frith@education.gov.uk',
+        password: 'Password',
+        isAdmin: true,
+      },
+      {
+        firstName: 'Dan',
+        username: 'dp-daly',
+        email: 'danpatdaly@gmail.com',
+        password: 'Password',
+        isAdmin: true,
+      },
+      {
+        firstName: 'Lana',
+        username: 'delrey',
+        email: 'lana@delrey.com',
+        password: 'Password',
+        isAdmin: false,
+      },
+      {
+        firstName: 'Gloria',
+        username: 'gloriosa',
+        email: 'gloria@gmail.com',
+        password: 'Password',
+        isAdmin: false,
+      }
+    )
   
-      console.log(`🧑‍🎨 ${adminUser.username} user created 🧑‍🎨`)
+      console.log(`🧑‍🎨 ${users.length} users created 🧑‍🎨`)
   
       const exhibitions = await Exhibition.create(exhibitionData)
   
