75c31a1de7ea7461be324bdc68e9723f63ad617f
Emma Frith <emma.frith@education.gov.uk>
2024-06-20 15:23:06 +0100

    add basic error handling for sign up page with toastify

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,79 +6,62 @@ import { secret } from '../config/environment.js'
 
 const router = express.Router()
 
-//lets use the errors from errors.js
-
 router.post('/signup', async (req, res, next) => {
-
     try {
         // ! 1) validation
         // ? we've been able to skip checking uniqueness of email/user due to unique validators in User model
         //check password/conf
-
         if (req.body.password !== req.body.passwordConfirmation) {
-            console.log('PASSWORDS DO NOTMATCH')
+            console.log('passwords do not match')
         }
-
         // ! 2) hash password
         const passwordHash = bcrypt.hashSync(req.body.password, 10)
         req.body.password = passwordHash
-
         // ! 3) save user to database
         const user = await User.create(req.body)
-
         res.status(201).json({
             message: `Welcome ${user.username}`
         })
-
-    } catch (err) {
-        next(err)
+    } catch (error) {
+        res.status(400).send(error)
     }
-
 })
 
 
 router.post('/signin', async (req, res, next) => {
-
     try {
         const user = await User.findOne({ email: req.body.email })
-
         if (!user) {
             console.log("NOT USER")
         }
-
         const passwordsMatch = await bcrypt.compare(
             req.body.password,
             user.password
         )
-
         if (!passwordsMatch) {
             console.log("PASSWORDS DONT MATCH")
         }
-
         // ! Create token to send back
         const token = jwt.sign(
-        //public payload
+            //public payload
             {
                 username: user.username,
                 userId: user._id,
                 isAdmin: user.isAdmin
-            }, 
-        //imported above from config/environment.js file
+            },
+            //imported above from config/environment.js file
             secret,
             {
                 expiresIn: "7d", //set expiry
             }
-    )
-
+        )
         res.send({
             message: 'Welcome back',
             token
         })
-
-    } catch (err) {
-        next(err)
+    } catch (error) {
+        res.status(400).send(error)
     }
-
 })
 
 
--- models/user.js
+++ models/user.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose'
 import mongooseUniqueValidator from 'mongoose-unique-validator'
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: false },
+    firstName: { type: String, required: true },
     lastName: { type: String, required: false },
     location: { type: String, required: false },
-    username: { type: String, unique: true, maxlength: 50, required: false },
+    username: { type: String, unique: true, maxlength: 50, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
