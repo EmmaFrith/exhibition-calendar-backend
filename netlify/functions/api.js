3a716281d28f1f9ba486ddd1faf1996cc525e909
Emma Frith <emma.frith@education.gov.uk>
2024-06-21 10:25:25 +0100

    get ready for deployment

---
+++ netlify/functions/api.js
@@ -0,0 +1,45 @@
+import 'dotenv/config';
+
+import { connectToDb } from './db/helpers.js';
+
+import express from 'express';
+
+
+import { dbURL, port } from '../../config/environment.js'; // Import path fixed
+
+
+
+import exhibitionRouter from '../../controllers/exhibition.js';
+
+import authRouter from '../../controllers/auth.js';
+
+import userRouter from '../../controllers/user.js';
+
+import cors from 'cors' // Added this new import
+import serverless from "serverless-http"  // added this new import
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors())
+
+app.use('/api', exhibitionRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+
+async function startServer() {
+  try {
+    await connectToDb();
+    console.log('Database connected');
+  } catch (err) {
+    console.log('Oh no something went wrong');
+    console.log(err);
+  }
+}
+
+startServer();
+
+export const handler = serverless(app)
+
